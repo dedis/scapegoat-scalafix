1ad1df27f5459fa77cb879665c17e6bb1fd4be8f
Thibault Czarniak <thibault.czarniak@epfl.ch>
2024-09-12 17:10:25 +0200

    Added CollectionNamingConfusion

---
+++ input/src/main/scala/fix/CollectionNamingConfusion.scala
@@ -0,0 +1,75 @@
+/*
+rule = CollectionNamingConfusion
+ */
+package fix
+
+object CollectionNamingConfusion {
+  def test(): Unit = {
+
+    // Incorrect names
+    val list = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val set = List(1,2,3) // assert: CollectionNamingConfusion
+    val vector = Seq(1,2,3) // assert: CollectionNamingConfusion
+    val seq = Vector(1,2,3) // assert: CollectionNamingConfusion
+    val array = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    val map = Array(1,2,3) // assert: CollectionNamingConfusion
+
+    // Longer incorrect names
+    val listSet = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val listVector = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+    val listSeq = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+    val listArray = Array(1, 2, 3) // assert: CollectionNamingConfusion
+    val listMap = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    var listSetVar = Set(1, 2, 3) // assert: CollectionNamingConfusion
+
+    val setList = List(1, 2, 3) // assert: CollectionNamingConfusion
+    val setVector = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+    val setSeq = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+    val setArray = Array(1, 2, 3) // assert: CollectionNamingConfusion
+    val setMap = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    var setVectorVar = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+
+    val vectorList = List(1, 2, 3) // assert: CollectionNamingConfusion
+    val vectorSet = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val vectorSeq = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+    val vectorArray = Array(1, 2, 3) // assert: CollectionNamingConfusion
+    val vectorMap = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    var vectorSeqVar = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+
+    val seqList = List(1, 2, 3) // assert: CollectionNamingConfusion
+    val seqSet = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val seqVector = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+    val seqArray = Array(1, 2, 3) // assert: CollectionNamingConfusion
+    val seqMap = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    var seqArrayVar = Array(1, 2, 3) // assert: CollectionNamingConfusion
+
+    val arrayList = List(1, 2, 3) // assert: CollectionNamingConfusion
+    val arraySet = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val arrayVector = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+    val arraySeq = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+    val arrayMap = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+    var arrayMapVar = Map(1 -> "one", 2 -> "two") // assert: CollectionNamingConfusion
+
+    val mapList = List(1, 2, 3) // assert: CollectionNamingConfusion
+    val mapSet = Set(1, 2, 3) // assert: CollectionNamingConfusion
+    val mapVector = Vector(1, 2, 3) // assert: CollectionNamingConfusion
+    val mapSeq = Seq(1, 2, 3) // assert: CollectionNamingConfusion
+    val mapArray = Array(1, 2, 3) // assert: CollectionNamingConfusion
+    var mapListVar = List(1, 2, 3) // assert: CollectionNamingConfusion
+
+    // Correct names
+    val myList = List(1, 2, 3) // scalafix: ok;
+    val mySet = Set(1, 2, 3) // scalafix: ok;
+    val myVector = Vector(1, 2, 3) // scalafix: ok;
+    val mySeq = Seq(1, 2, 3) // scalafix: ok;
+    val myArray = Array(1, 2, 3) // scalafix: ok;
+    val myMap = Map(1 -> "one", 2 -> "two") // scalafix: ok;
+    var myListVar = List(1, 2, 3) // scalafix: ok;
+
+    // Normal names
+    val currentLBListenerSettings = List(1) // scalafix: ok;
+    val exclusionsForAdvancedSetup = List(2, 3) // scalafix: ok;
+    val preSetupSteps = List(4) // scalafix: ok;
+  }
+
+}
---
+++ rules/src/main/scala/fix/CollectionNamingConfusion.scala
@@ -0,0 +1,40 @@
+/*
+rule = CollectionNamingConfusion
+ */
+package fix
+
+import scalafix.lint.LintSeverity
+import scalafix.v1._
+
+import scala.meta._
+
+class CollectionNamingConfusion extends SemanticRule("CollectionNamingConfusion") {
+
+  private def diag(pos: Position) = Diagnostic(
+    "",
+    "Checks for variables that are confusingly named.",
+    pos,
+    "E.g. an instance of a Set is confusingly referred to by a variable called/containing list, or the other way around.",
+    LintSeverity.Info
+  )
+
+  override def fix(implicit doc: SemanticDocument): Patch = {
+
+    def leaveOneOut(name: String): Set[String] = name match {
+      case "list"   => Set("set", "vector", "seq", "array", "map")
+      case "set"    => Set("list", "vector", "seq", "array", "map")
+      case "vector" => Set("list", "set", "seq", "array", "map")
+      case "seq"    => Set("list", "set", "vector", "array", "map")
+      case "array"  => Set("list", "set", "vector", "seq", "map")
+      case "map"    => Set("list", "set", "vector", "seq", "array")
+      case _        => Set.empty
+    }
+
+    doc.tree.collect {
+      // Corresponds to vals / vars of collection types that have a confusing name (i.e. of another collection) e.g. val set = List(1,2,3) or var listSet =
+      case t@Defn.Val(_, List(Pat.Var(Term.Name(qual))), _, Term.Apply.After_4_6_0(Term.Name(collName), _)) if leaveOneOut(collName.toLowerCase()).exists(qual.contains) => Patch.lint(diag(t.pos))
+      case t@Defn.Var.After_4_7_2(_, List(Pat.Var(Term.Name(qual))), _, Term.Apply.After_4_6_0(Term.Name(collName), _)) if leaveOneOut(collName.toLowerCase()).exists(qual.contains) => Patch.lint(diag(t.pos))
+    }.asPatch
+  }
+
+}
