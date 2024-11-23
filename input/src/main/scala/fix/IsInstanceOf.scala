3d438215a7d2b4e8205a6ecd299c952550e12e1d
Thibault Czarniak <thibault.czarniak@epfl.ch>
2024-11-23 10:12:13 +0200

    Scalaformatted

--- input/src/main/scala/fix/IsInstanceOf.scala
+++ input/src/main/scala/fix/IsInstanceOf.scala
@@ -11,9 +11,9 @@ object IsInstanceOf {
     case class MappingCharFilter(name: String, mappings: (String, String)*) // scalafix: ok;
 
     val pf: PartialFunction[Any, Unit] = {
-      case s: String => println(s) // scalafix: ok;
+      case s: String        => println(s) // scalafix: ok;
       case i: Int if i == 4 => println(i) // scalafix: ok;
-      case _ => println("no match :(") // scalafix: ok;
+      case _                => println("no match :(") // scalafix: ok;
     }
 
     @SuppressWarnings(Array("all"))
@@ -28,14 +28,13 @@ object IsInstanceOf {
       println(s.isInstanceOf[String]) // scalafix: ok;
     }
 
-
     sealed trait MyGADT[T]
     final case class VariantInt(value: Int) extends MyGADT[Int]
     final case class VariantString(value: String) extends MyGADT[String]
 
     def doStuff[T](gadt: MyGADT[T]): T = { // scalafix: ok;
       gadt match {
-        case VariantInt(value) => value
+        case VariantInt(value)    => value
         case VariantString(value) => value
       }
     }
